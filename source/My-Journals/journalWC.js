e696d3a44575d72b22f90c94acca7f5d97889398
ethanwads <eswadswo@ucsd.edu>
2021-05-17 18:25:54 -0700

    test ci-cd

---
+++ source/My-Journals/journalWC.js
@@ -0,0 +1,11 @@
+class journalWC extends HTMLElement {
+    constructor() {
+        super();
+
+        const template = document.createElement('template');
+
+        template.innerHTML = `
+        
+        `
+    }
+}
