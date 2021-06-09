251fa4b6fcbc4de2e9dcfcc3cc2d800a097efe7a
ethanwads <eswadswo@ucsd.edu>
2021-06-09 14:51:02 -0700

    removed test

--- source/My-Journals/__tests__/my-journals.test.js
+++ source/My-Journals/__tests__/my-journals.test.js
@@ -1,16 +1,16 @@
-const { expect, test } = require('@jest/globals');
-// const {parseColor} = require('../my-journals-script.js');
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
-import {parseColor} from '../my-journals-script.js';
+// const { expect, test } = require('@jest/globals');
+// // const {parseColor} = require('../my-journals-script.js');
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import "firebase/database";
+// import {parseColor} from '../my-journals-script.js';
 
-test('test color', () => {
-    let color = parseColor('red');
+// test('test color', () => {
+//     let color = parseColor('red');
 
-    expect(color).toBe('#EF6666');
-})
+//     expect(color).toBe('#EF6666');
+// })
 
-test('test', () => {
-    expect(2).toBe(2);
-})
+// test('test', () => {
+//     expect(2).toBe(2);
+// })
